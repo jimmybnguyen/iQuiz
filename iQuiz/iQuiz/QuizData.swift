1bbbde8e232d81e70632ea3fe4f274ffb3765cba
jimmybnguyen <jimmyn9@uw.edu>
2017-11-17 16:34:36 -0800

    finished data pulling and saving

---
+++ iQuiz/iQuiz/QuizData.swift
@@ -0,0 +1,53 @@
+//
+//  Data.swift
+//  iQuiz
+//
+//  Created by Jimmy Nguyen on 11/16/17.
+//  Copyright © 2017 Jimmy B Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class QuizData: NSObject {
+    
+    static let shared = QuizData()
+    var urlSource: String = "https://tednewardsandbox.site44.com/questions.json"
+    var data: [[String:Any]]?
+    var fileURL : URL?
+    
+    override init() {
+        super.init()
+        let DocumentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        fileURL = DocumentDirURL.appendingPathComponent("quiz-data").appendingPathExtension("json")
+        load()
+    }
+    
+    func load() {
+        do{
+            var content = NSData(contentsOf: URL(string: urlSource)!)
+            if (content == nil) {
+                do {
+                    content = try NSData(contentsOf: fileURL!)
+                } catch {
+                    print(error)
+                }
+            }
+            if (content != nil) {
+                data = try JSONSerialization.jsonObject(with: content as! Data, options: []) as! [[String:Any]]
+                save(content!)
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
+    func save(_ content:NSData){
+        do {
+            if let questions = String(data: content as Data, encoding: String.Encoding.utf8) {
+                try questions.write(to: fileURL!, atomically: true, encoding: String.Encoding.utf8)
+            }
+        } catch {
+            print(error)
+        }
+    }
+}
--- iQuiz/iQuiz/ViewController.swift
+++ iQuiz/iQuiz/ViewController.swift
@@ -12,99 +12,31 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     var selectedQuestions:[Any]?
     
-    let questions =
-        ["Mathematics":
-            ["description":"Did you pass the third grade?",
-             "icon": "Math.png",
-             "questions":
-                [
-                    [
-                        "text": "What is 2+2?",
-                        "answer": "1",
-                        "answers": [
-                            "4",
-                            "22",
-                            "An irrational number",
-                            "Nobody knows"
-                        ]
-                    ]
-                ],
-            ],
-         "Marvel Super Heroes":
-            ["description":"Avengers, Assemble!",
-             "icon": "Marvel.png",
-             "questions":
-                [
-                    [
-                        "text": "Who is Iron Man?",
-                        "answer": "1",
-                        "answers": [
-                            "Tony Stark",
-                            "Obadiah Stane",
-                            "A rock hit by Megadeth",
-                            "Nobody knows"
-                        ]
-                    ],
-                    [
-                        "text": "Who founded the X-Men?",
-                        "answer": "2",
-                        "answers": [
-                            "Tony Stark",
-                            "Professor X",
-                            "The X-Institute",
-                            "Erik Lensherr"
-                            ]
-                        ],
-                        [
-                        "text": "How did Spider-Man get his powers?",
-                        "answer": "1",
-                        "answers": [
-                        "He was bitten by a radioactive spider",
-                        "He ate a radioactive spider",
-                        "He is a radioactive spider",
-                        "He looked at a radioactive spider"
-                        ]
-                    ]
-                ],
-            ],
-         "Science!":
-            ["description":"Because SCIENCE!",
-             "icon": "Science.png",
-             "questions":
-                [
-                    [
-                        "text": "What is fire?",
-                        "answer": "1",
-                        "answers": [
-                            "One of the four classical elements",
-                            "A magical reaction given to us by God",
-                            "A band that hasn't yet been discovered",
-                            "Fire! Fire! Fire! heh-heh"
-                        ]
-                    ],
-                ],
-            ]
-        ]
+    var questions : [[String:Any]]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if (QuizData.shared.data != nil) {
+            questions = QuizData.shared.data!
+        }
+    }
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return questions.count
+        return questions!.count
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ViewControllerTableViewCell
-        let keys = Array(questions.keys)[indexPath.row]
-        let subjects = questions[keys]! as [String:Any]
-        cell.myTitle.text = keys
-        cell.myDesc.text = subjects["description"] as! String
-        cell.myImage.image = UIImage(named: subjects["icon"] as! String)
+        let dictionary = questions![indexPath.row] as [String:Any]
+        cell.myTitle.text = dictionary["title"] as! String
+        cell.myDesc.text = dictionary["desc"] as! String
+        cell.myImage.image = UIImage(named: "quizicon")
         return cell
     }
     
     public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let cell = tableView.cellForRow(at: indexPath) as! ViewControllerTableViewCell
-        let key = cell.myTitle.text!
-        let dictionary = questions[key]! as [String:Any]
-        selectedQuestions = dictionary["questions"] as! [Any]
+        let dictionary = questions![indexPath.row] as [String:Any]
+        selectedQuestions = dictionary["questions"] as! [[String:Any]]
         performSegue(withIdentifier: "questionSegue", sender: self)
     }
     
@@ -119,10 +51,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let alert = UIAlertController(title: "Settings", message: "Settings go here", preferredStyle: UIAlertControllerStyle.alert)
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
         self.present(alert, animated: true, completion: nil)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
     }
 
     override func didReceiveMemoryWarning() {
